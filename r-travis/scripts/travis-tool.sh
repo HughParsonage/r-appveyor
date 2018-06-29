ad5ba359c0fb1f048d41f9b9cc9fd3e619a36e04
HughParsonage <hugh.parsonage@gmail.com>
2018-06-29 11:29:17 +1000

    Update travis-tool.sh

--- r-travis/scripts/travis-tool.sh
+++ r-travis/scripts/travis-tool.sh
@@ -275,7 +275,7 @@ DumpLogs() {
 }
 
 RunTests() {
-    wget http://mirror.ctan.org/systems/win32/miktex/setup/miktexsetup-x64.zip
+    wget https://ctan.math.illinois.edu/systems/win32/miktex/setup/miktexsetup-x64.zip
     unzip miktexsetup-x64.zip
     miktexsetup --verbose --local-package-repository=C:\temp\miktex --package-set=basic download
     miktexsetup --verbose --local-package-repository=C:\temp\miktex --modify-path install    
