38ebccc9a408721e5ca68683d4e9ec19626901ec
HughParsonage <hugh.parsonage@gmail.com>
2018-06-29 11:15:31 +1000

    Add LaTeX for run tests

--- r-travis/scripts/travis-tool.sh
+++ r-travis/scripts/travis-tool.sh
@@ -275,6 +275,11 @@ DumpLogs() {
 }
 
 RunTests() {
+    wget http://mirror.ctan.org/systems/win32/miktex/setup/miktexsetup-x64.zip
+    unzip miktexsetup-x64.zip
+    miktexsetup --verbose --local-package-repository=C:\temp\miktex --package-set=basic download
+    miktexsetup --verbose --local-package-repository=C:\temp\miktex --modify-path install    
+
     echo "Building with: R CMD build ${R_BUILD_ARGS}"
     if [[ "${OS:0:5}" == "MINGW" || "${OS:0:4}" == "MSYS" ]]; then
         if [[ -d vignettes ]]; then
